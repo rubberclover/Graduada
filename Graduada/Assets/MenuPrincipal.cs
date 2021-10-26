539d95cff9e3f7000263c53d663f0f3b19ddf825
LrRuben <38402745+LrRuben@users.noreply.github.com>
2021-10-26 20:52:56 +0200

    Logica de cambios de nivel

--- Graduada/Assets/MenuPrincipal.cs
+++ Graduada/Assets/MenuPrincipal.cs
@@ -6,8 +6,10 @@ using UnityEngine.SceneManagement;
 public class MenuPrincipal : MonoBehaviour
 {
     // Start is called before the first frame update
+    LogicaCambioNivel nivel = new LogicaCambioNivel();
     public void PlayGame(){
-      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+      //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+      nivel.comenzar();
     }
 
     public void QuitGame(){
