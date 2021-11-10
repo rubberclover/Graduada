a39719ca53d27e21234c13a9220727043483d348
Jnegrillop <jnegrillopastor@hotmail.com>
2021-11-10 19:05:09 +0100

    Cambios

--- Graduada/Assets/Scripts/Controllers/EnemyMovement.cs
+++ Graduada/Assets/Scripts/Controllers/EnemyMovement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Sectario : MonoBehaviour
+public class EnemyMovement : MonoBehaviour
 {
     public float lookRadius = 10f;
 
