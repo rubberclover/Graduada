bc9cb9fbfc19a09ab434c5809f610a1663e0eb40
LrRuben <38402745+LrRuben@users.noreply.github.com>
2021-11-15 19:19:35 +0100

    Revert "Merge branch 'main' of https://github.com/rubberclover/Graduada"
    This reverts commit 608a077f50091640c293f0b60292cb7640594b35, reversing
    changes made to c391305273c92780335819937c090b1e71e9df94.

--- Graduada/Assets/Scenes/Pruebas/Generacion mundo/Scripts/Spawner.cs
+++ Graduada/Assets/Scenes/Pruebas/Generacion mundo/Scripts/Spawner.cs
@@ -2,50 +2,81 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class Spawner : MonoBehaviour
 {
-
-   
     public int direccion;
     // Puertas que tiene que tener la habitaci�n que aparecer� en ese punto
-    // 1 = Puerta hacia abajo
-    // 2 = Puerta hacia arriba
-    // 3 = Puerta hacia la izquierda
-    // 4 = Puerta hacia la derecha
+    // 1 = Puerta hacia abajo (++)
+    // 2 = Puerta hacia arriba (--)
+    // 3 = Puerta hacia la izquierda (++)
+    // 4 = Puerta hacia la derecha (--)
     private Templates templates;
-    public int rand;
+    private int rand;
+    private int maxHab;
+    private int tamano;
     private bool spawned = false;
     void Start()
     {
         templates = GameObject.FindGameObjectWithTag("ListasHabitaciones").GetComponent<Templates>();
+        maxHab = templates.maxHab;
         Invoke("Spawn", 0.1f);
-    
     }
     void Spawn()
     {
         if (spawned==false){
-            // Generamos las habitaciones
+            // Generamos habitaciones arriba (necesita puerta abajo)
             if (direccion == 1)
             {
-                // Añadir contador para tamaño máximo de mapa
-                rand = Random.Range(0, templates.BottomRooms.Length);
-                Instantiate(templates.BottomRooms[rand], transform.position, templates.BottomRooms[rand].transform.rotation);
+                tamano = templates.BottomRooms.Length;
+                if(Templates.numHab == maxHab){
+                    Debug.Log("Arriba max");
+                    Instantiate(templates.BottomRooms[tamano-1], transform.position, templates.BottomRooms[tamano-1].transform.rotation);
+                }
+                else{
+                    Debug.Log("Arriba");
+                    rand = Random.Range(0, tamano);
+                    Instantiate(templates.BottomRooms[rand], transform.position, templates.BottomRooms[rand].transform.rotation);
+                    Templates.numHab+=1;
+                }
             }
             else if (direccion == 2)
             {
-                rand = Random.Range(0, templates.TopRooms.Length);
-                Instantiate(templates.TopRooms[rand], transform.position, templates.TopRooms[rand].transform.rotation);
+                tamano = templates.TopRooms.Length;
+                if(Templates.numHab == maxHab){
+                    Debug.Log("Abajo max");
+                    Instantiate(templates.TopRooms[tamano -1], transform.position, templates.TopRooms[tamano -1].transform.rotation);
+                }else{
+                    Debug.Log("Abajo");
+                    rand = Random.Range(0,tamano);
+                    Instantiate(templates.TopRooms[rand], transform.position, templates.TopRooms[rand].transform.rotation);
+                    Templates.numHab+=1;
+                }
             }
             else if (direccion == 3)
             {
-                rand = Random.Range(0, templates.LeftRooms.Length);
-                Instantiate(templates.LeftRooms[rand], transform.position, templates.LeftRooms[rand].transform.rotation);
+                tamano = templates.LeftRooms.Length;
+                if(Templates.numHab == maxHab){
+                    Debug.Log("Der max");
+                    Instantiate(templates.LeftRooms[tamano-1], transform.position, templates.LeftRooms[tamano-1].transform.rotation);
+                }else{
+                    Debug.Log("Derecha");
+                    rand = Random.Range(0, tamano);
+                    Instantiate(templates.LeftRooms[rand], transform.position, templates.LeftRooms[rand].transform.rotation);
+                    Templates.numHab+=1;
+                }
             }
             else
             {
-                rand = Random.Range(0, templates.RightRooms.Length);
-                Instantiate(templates.RightRooms[rand], transform.position, templates.RightRooms[rand].transform.rotation);
+                tamano = templates.RightRooms.Length;
+                if(Templates.numHab == maxHab){
+                    Debug.Log("Izq max");
+                    Instantiate(templates.RightRooms[tamano-1], transform.position, templates.RightRooms[tamano-1].transform.rotation);
+                }else{
+                    Debug.Log("Izquierda");
+                    rand = Random.Range(0, tamano);
+                    Instantiate(templates.RightRooms[rand], transform.position, templates.RightRooms[rand].transform.rotation);
+                    Templates.numHab+=1;
+                }
             }
             spawned = true;
      
---
+++ Graduada/Assets/Scenes/Pruebas/Generacion mundo/Scripts/camera.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    public Transform PlayerTransform;
+    private Vector3 _cameraOffset; 
+
+    [Range(0.01f, 1.0f)]
+    public float SmoothFactor = 0.5f;
+
+    
+    void Start()
+    {
+        _cameraOffset = transform.position - PlayerTransform.position;
+    }
+
+    void Update()
+    {
+        Vector3 newPos = PlayerTransform.position + _cameraOffset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
+    }
+}
