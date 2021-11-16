f57a167e8926ea0dd332ea87698f3914901d6da5
rubberclover <marlite98@gmail.com>
2021-11-16 21:20:40 +0100

    Añadido vida

--- Graduada/Assets/Scenes/Pruebas/Vida/ProtagonistaVida.cs
+++ Graduada/Assets/Scenes/Pruebas/Vida/ProtagonistaVida.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ProtagonistaVida : MonoBehaviour
 {
+    public Image[] corazones;
     public int health;
 
     //100 health 
 
-    public void LoseHealth(int value){
-        health -= value;
+    public void LoseHealth(){
+        health--;
+        corazones[health].enabled = false;
+
+        if(health==0){
+          Debug.Log("You lose");
+        }
     }
 
     public void GainHealth(int value){
        health += value; 
+    }
+     private void Update() {
+        if(Input.GetKeyDown(KeyCode.M)){
+        LoseHealth();
+        }
     }
 }
