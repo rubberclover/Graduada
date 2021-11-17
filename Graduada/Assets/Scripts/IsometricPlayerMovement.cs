9669d3a552d3e120f47d122209c2c6c2293744e7
LrRuben <38402745+LrRuben@users.noreply.github.com>
2021-11-17 18:55:28 +0100

    Arreglado movimiento

--- Graduada/Assets/Scripts/IsometricPlayerMovement.cs
+++ Graduada/Assets/Scripts/IsometricPlayerMovement.cs
@@ -1,76 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
- 
-public class IsometricPlayerMovement : MonoBehaviour {
- 
-    [SerializeField]
-    float moveSpeed = 12f;
- 
-    Vector3 forward, right;
 
-    bool jump = false;
-    
+public class IsometricPlayerMovement : MonoBehaviour
+{   
+    CharacterController characterController;
+
     [SerializeField]
-    float jumpHeight = 8f, jumpSpeed = 15f;
-    Rigidbody rigidbody;
- 
-    void Start ()
+    public float speed = 12;
+    [SerializeField]
+    public float jumpSpeed = 16.0f;
+    [SerializeField]
+    public float gravity = 40.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private Vector3 forward, right;
+
+    void Start()
     {
+        characterController = GetComponent<CharacterController>();
         forward = Camera.main.transform.forward;
         forward.y = 0;
         forward = Vector3.Normalize(forward);
         right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
-        rigidbody = GetComponent<Rigidbody>();
     }
- 
- 
-    void Update ()
+
+    void Update()
     {
-        if (Input.GetButtonDown("Jump") && !jump){
-            StartCoroutine(Jump());
-        }else Move();
-    }
- 
-    void Move()
-    {
-        Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("HorizontalStreet");
-        Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("VerticalStreet");
- 
-        Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
- 
-        transform.forward = heading;
-        transform.position += rightMovement;
-        transform.position += upMovement;
-    }
-
-    IEnumerator Jump(){
-        float originalHeight = transform.position.y;
-        float maxHeight = originalHeight + jumpHeight;
-        rigidbody.useGravity = false;
-
-        jump = true;
-        yield return null;
-
-
-        while(transform.position.y < maxHeight)
+        if (characterController.isGrounded)
         {
-            transform.position += transform.up * Time.deltaTime * jumpSpeed; 
-            yield return null;
+            Vector3 rightMovement = right * speed * Time.deltaTime * Input.GetAxis("HorizontalStreet");
+            Vector3 upMovement = forward * speed * Time.deltaTime * Input.GetAxis("VerticalStreet");
+ 
+
+            moveDirection = Vector3.Normalize(rightMovement + upMovement);
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
         }
 
-        //rigidbody.useGravity = true;
+        moveDirection.y -= gravity * Time.deltaTime;
 
-        while(transform.position.y > originalHeight){
-            transform.position -= transform.up * Time.deltaTime * jumpSpeed; //si la gravedad está activada, desactivar
-            yield return null;
-        }
-
-        rigidbody.useGravity = true;
-        jump = false;
-
-        yield return null;
+        characterController.Move(moveDirection * Time.deltaTime);
     }
 }
 
-     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
