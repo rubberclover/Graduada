24b3071487e9576823dc7cba54b66c2516e8c39d
rubberclover <marlite98@gmail.com>
2022-01-07 17:22:52 +0100

    Añadido YarnSpinner

---
+++ Graduada/Packages/YarnSpinner-Unity-2.0.1/Runtime/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyVersion("2.0")]
+[assembly: AssemblyInformationalVersion("2.0.0-beta2")]
+[assembly: InternalsVisibleTo("YarnSpinnerTests")]
+[assembly: InternalsVisibleTo("YarnSpinner.Unity.Editor")]
+
