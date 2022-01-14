0e3d7bb461e7ee0f69a13b7fb6c7f0a1f1dd5d7f
LrRuben <38402745+LrRuben@users.noreply.github.com>
2022-01-14 20:59:40 +0100

    Revert "Merge branch 'main' of https://github.com/rubberclover/Graduada"
    This reverts commit 8b7a9dd9a48a63f21fb4dd5003f1d941eb23464e, reversing
    changes made to a42dc1e21f318a3e71d572819e99e70bb07a40f4.

--- Graduada/Assets/Scenes/Pruebas/Generacion mundo/Scripts/Templates.cs
+++ Graduada/Assets/Scenes/Pruebas/Generacion mundo/Scripts/Templates.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Templates : MonoBehaviour
 {
@@ -12,16 +11,4 @@ public class Templates : MonoBehaviour
     public GameObject enemy;
     public int maxHab;
     public static int numHab;
-    public static bool bar;
-    void Start(){
-            StartCoroutine(esperar(10));
-            
-        }
-        private IEnumerator esperar(int segundos){
-            yield return new WaitForSeconds(segundos);
-            if(bar == false){
-                SceneManager.LoadScene(4);
-            }
-        }
-        
 }
